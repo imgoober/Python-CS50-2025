5d777eff92a00ea87fca6a160cf87c31f81e0cd7
Rahul <rahulp2809@gmail.com>
2026-01-01 20:10:50 -0800

    Add files via upload

---
+++ pset0/indoor/indoor.py
@@ -0,0 +1,3 @@
+user_input = input("Enter message:")
+
+print(user_input.lower())
---
+++ pset0/playback/playback.py
@@ -0,0 +1,4 @@
+user_input = input("Enter something: ")
+
+playback_output = user_input.replace(' ', "...")
+print (playback_output)
